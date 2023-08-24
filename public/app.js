afcb5c0de8328f9b896a5fe9509cf5899e755a00
thejobbyjohnson <jobbyjohnson1998@gmail.com>
2023-08-24 20:46:35 +0800

    feat: Added functionality to detect emojis

--- public/app.js
+++ public/app.js
@@ -11,18 +11,41 @@ usernameInput.addEventListener('change', () => {
   username = usernameInput.value.trim();
 });
 
-sendButton.addEventListener('click', () => {
-  const message = messageInput.value.trim();
-  if (message !== '' && username !== '') {
-    socket.emit('message', { username, message }); // Send both username and message
-    messageInput.value = '';
-  }
-});
+// ... (previous code) ...
 
+messageInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault(); // Prevent newline from being entered in the textarea
+      sendButton.click(); // Trigger the send button's click event
+    }
+  });
+  
+  sendButton.addEventListener('click', () => {
+    const message = messageInput.value.trim();
+    if (message !== '' && username !== '') {
+      const messageWithEmojis = replaceEmojis(message);
+      socket.emit('message', { username, message: messageWithEmojis });
+      messageInput.value = '';
+    }
+  });
+  
 socket.on('message', (data) => {
   const messageElement = document.createElement('div');
   messageElement.className = 'message';
-  messageElement.textContent = `${data.username}: ${data.message}`; // Display username with the message
+  messageElement.innerHTML = `<strong>${data.username}:</strong> ${replaceEmojis(data.message)}`;
   messagesContainer.appendChild(messageElement);
   messagesContainer.scrollTop = messagesContainer.scrollHeight;
 });
+
+function replaceEmojis(message) {
+  const emojiMap = {
+    react: "⚛",
+    woah: "😮",
+    hey: "👋",
+    lol: "😂",
+    like: "❤️",
+    congratulations: "🎉",
+  };
+
+  return message.replace(/\b(react|woah|hey|lol|like|congratulations)\b/gi, (match) => emojiMap[match.toLowerCase()] || match);
+}
